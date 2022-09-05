8be8713b954f334a07499a659d455613f81d26f5
정보통신-이병석 <byoungseok.lee@lotte.net>
2022-09-05 16:18:27 +0900

    2.metadata

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -2,14 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownalbe.sol";
 
-contract MintGemToken is ERC721Enumerable {
+contract MintGemToken is ERC721Enumerable , Ownable{
+    string public metadataURI;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name,_symbol){}
+    constructor(string memory _name, string memory _symbol,string memory _metadataURI) ERC721(_name,_symbol){
+        metadataURI = _metadataURI;
+    }
 
-    function MintGemToken() public {
+    function tokenURI(uint256 _tokenId) override public view returns (string memory)  {
+        string memory str_id = Strings.toString(_tokenId);
+
+        return string(abi.encodePacked(metadataURI,'/',str_id,'.json'));
+    }
+
+    function mintGemToken() public {
        uint tokenId = totalSupply() + 1;
 
         _mint(msg.sender, tokenId);
     }
+
+
 }
